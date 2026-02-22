7f7fd76d978220d7c022582ca71df6a84ed66ece
homocodian <homocodian@gmail.com>
2026-02-23 00:43:00 +0530

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/homocodian/404-hunter
+
+go 1.25.0
+
+require golang.org/x/net v0.50.0
---
+++ static/index.js
@@ -0,0 +1,68 @@
+const form = document.getElementById("url-checker");
+const button = document.querySelector("button[type='submit']");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const url = new FormData(form).get("url");
+  if (!url) {
+    alert("Please enter a URL");
+    return;
+  }
+
+  if (!URL.canParse(url)) {
+    alert("Invalid URL");
+    return;
+  }
+
+  parsedUrl = new URL(url);
+  if (!parsedUrl.protocol.startsWith("http")) {
+    alert("Invalid URL");
+    return;
+  }
+  if (!parsedUrl.hostname) {
+    alert("Invalid URL");
+    return;
+  }
+
+  event.currentTarget.reset();
+
+  button.disabled = true;
+  const prompt = document.createElement("strong");
+  prompt.textContent = "Checking...";
+  document.body.appendChild(prompt);
+
+  fetch("/check", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ url }),
+  })
+    .then(async (response) => {
+      if (!response.ok) {
+        const errorText = await response.text().catch(() => "Unknown error");
+        throw new Error(`HTTP error! status: ${response.status}, ${errorText}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const strong = document.createElement("strong");
+      strong.textContent = `Found ${data.deadLinks.length} dead links`;
+
+      const ul = document.createElement("ul");
+      data.deadLinks.forEach((link) => {
+        const li = document.createElement("li");
+        li.textContent = link;
+        ul.appendChild(li);
+      });
+
+      document.body.appendChild(strong);
+      document.body.appendChild(ul);
+    })
+    .catch((error) => alert(`Error fetching URL: ${error}`))
+    .finally(() => {
+      button.disabled = false;
+      document.body.removeChild(prompt);
+    });
+});
